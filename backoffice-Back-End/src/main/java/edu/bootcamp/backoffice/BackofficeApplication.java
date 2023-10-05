f6faa98f745468f4449f60fa7494dd05af1a8cad
Martin-Paez <mpaez.ok@gmail.com>
2023-10-05 20:56:01 -0300

    Proyect template

---
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/BackofficeApplication.java
@@ -0,0 +1,34 @@
+package edu.bootcamp.backoffice;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class BackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(Arrays.asList("*"));
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter();
+	}
+}
---
+++ backoffice-Front-End/backoffice-web/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="alert alert-danger">
+      <h1>Hello Word</h1>
+    </div>
+  );
+}
+
+export default App;
