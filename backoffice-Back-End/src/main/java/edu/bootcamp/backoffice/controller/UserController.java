4cd6cb288ecf41b9aff31b2a1ab5596062c090db
Martin-Paez <mpaez.ok@gmail.com>
2023-10-06 14:54:17 -0300

    UserController.v4: API 200s codes, fix PostmanCollection

--- backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/UserController.java
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/UserController.java
@@ -4,8 +4,11 @@ import edu.bootcamp.backoffice.model.dto.CreatUserRequestDTO;
 import edu.bootcamp.backoffice.model.dto.UserDTO;
 import edu.bootcamp.backoffice.service.UserService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,49 +23,91 @@ public class UserController {
         //this.userService = userService;
     }
 
-    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO saveUser(@RequestBody CreatUserRequestDTO userDTO)
+    @PostMapping(
+            value = "",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<UserDTO> saveUser(
+            @RequestBody CreatUserRequestDTO userDTO
+    )
     {
-        return new UserDTO(
+        UserDTO user = new UserDTO(
                 userDTO.getUsername(),
                 userDTO.getPassword(),
                 1
         );
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getUserId())
+                .toUri();
+        return ResponseEntity.created(location).body(user);
         //return this.userService.saveUser(userDTO);
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO getUser(@PathVariable int id)
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<UserDTO> getUser(@PathVariable int id)
     {
-        return new UserDTO("unUsername","unPass",id);
+        UserDTO user = new UserDTO(
+                "unUsername",
+                "unPass",
+                id
+        );
+        return ResponseEntity.ok(user);
         //throw new UnsupportedOperationException("endpoint getUser no implementado");
     }
 
-    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UserDTO> getAllUsers()
+    @GetMapping(
+            value = "",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<UserDTO>> getAllUsers()
     {
         int id = 1;
         List<UserDTO> users = new ArrayList<UserDTO>();
-        users.add(new UserDTO("user"+id,"pass"+id, id++));
-        users.add(new UserDTO("user"+id,"pass"+id, id++));
-        return users;
+        users.add(new UserDTO(
+                "user"+id,
+                "pass"+id,
+                id++
+        ));
+        users.add(new UserDTO(
+                "user"+id,
+                "pass"+id,
+                id
+        ));
+        return ResponseEntity.ok(users);
         //return this.userService.getListAllUsersInBD();
     }
 
-    @PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO updateUser(@RequestBody UserDTO userDTO)
+    @PutMapping(
+            value = "",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO)
     {
-        return new UserDTO(
+        UserDTO user = new UserDTO(
                 userDTO.getUsername(),
                 userDTO.getPassword(),
-                userDTO.getUserId());
+                userDTO.getUserId()
+        );
+        return ResponseEntity.ok(user);
         //throw new UnsupportedOperationException("endpoint updateUser no implementado");
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO deleteUser(@PathVariable int id)
+    public ResponseEntity<Void> deleteUser(@PathVariable int id)
     {
-        return new UserDTO("unUsername","unPass",id);
+        UserDTO user = new UserDTO(
+                "unUsername",
+                "unPass",
+                id
+        );
+        return ResponseEntity.noContent().build();
         //throw new UnsupportedOperationException("endpoint deleteUser no implementado");
     }
 
