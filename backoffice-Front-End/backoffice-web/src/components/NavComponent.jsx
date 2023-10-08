e301317f8b55bf6b21f7e79086b3a8ec38cc7cd6
luciano malleret <lucianomalleret8@gmail.com>
2023-10-08 10:48:06 -0300

    Subiendo modificaciones del NavComponent por temas de errores de diseño

--- backoffice-Front-End/backoffice-web/src/components/NavComponent.jsx
+++ backoffice-Front-End/backoffice-web/src/components/NavComponent.jsx
@@ -11,46 +11,42 @@ export function NavComponent() {
     return (
         <div class="container-fluid">
             <div class="row flex-nowrap">
-                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-primary">
-                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                        <div href="/" class="mt-4 container d-flex align-items-center justify-content-center p-0">
-                            <img src={imageLogoFinnegans} alt="Logo Finnegans" width="70" height="70" />
-                            <span class="fs-3 d-none d-sm-inline text-dark fw-bold">Finnegans</span>
-                        </div>
-                        <span class="d-none d-sm-inline mx-auto text-dark fw-normal fs-5" style={{ letterSpacing: ".3rem" }}>BACKOFFICE</span>
-                        <div class="container d-flex flex-column mt-5 pt-5">
-
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faHouse} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Perfil</span>
-                            </a>
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faUser} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Clientes</span>
-                            </a>
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faUsers} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Clientes</span>
-                            </a>
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faBox} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Productos</span>
-                            </a>
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faBellConcierge} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Servicios</span>
-                            </a>
-                            <a href="/" class="mb-3 bg-hover">
-                                <FontAwesomeIcon icon={faFileInvoiceDollar} size="xl" style={{ color: "#000000", width: "30px" }} />
-                                <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Cargos</span>
-                            </a>
-
-                        </div>
-                        <hr />
+                <div class="col-auto col-md-3 col-xl-2 px-0 bg-primary min-vh-100 d-flex flex-column align-items-center align-items-sm-start">
+                    <div href="/" class="mt-4 container d-flex align-items-center justify-content-center p-0">
+                        <img src={imageLogoFinnegans} alt="Logo Finnegans" width="70" height="70" />
+                        <span class="fs-3 d-none d-sm-inline text-dark fw-bold">Finnegans</span>
                     </div>
+                    <span class="d-none d-sm-inline mx-auto text-dark fw-normal fs-5" style={{ letterSpacing: ".3rem" }}>BACKOFFICE</span>
+                    <hr class="mb-5"/>
+                    <hr class="mb-3"/>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faHouse} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Inicio</span>
+                    </a>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faUser} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Clientes</span>
+                    </a>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faUsers} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Clientes</span>
+                    </a>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faBox} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Productos</span>
+                    </a>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faBellConcierge} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Servicios</span>
+                    </a>
+                    <a href="/" class="mb-3 ps-4 py-2 bg-hover container">
+                        <FontAwesomeIcon icon={faFileInvoiceDollar} size="xl" style={{ color: "#000000", width: "30px" }} />
+                        <span class="ms-3 d-none d-sm-inline text-dark fw-normal fs-5 align-middle">Cargos</span>
+                    </a>
+
                 </div>
-                <div class="col py-3">
-                </div>
+            </div>
+            <div class="col py-3">
             </div>
         </div>
     );
