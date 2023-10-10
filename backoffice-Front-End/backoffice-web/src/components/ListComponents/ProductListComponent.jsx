70293d5b61251ef1d8fbc201cbf8c263ec9f8be5
Marcos Leiva <marcleiva623@gmail.com>
2023-10-10 20:13:17 -0300

    Adaptaciones de las listas (Botón Edit)

--- backoffice-Front-End/backoffice-web/src/components/ListComponents/ProductListComponent.jsx
+++ backoffice-Front-End/backoffice-web/src/components/ListComponents/ProductListComponent.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import NavComponent from '../NavComponent'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 function ProductListComponent() {
     const [Products, setProducts] = useState([])
 
-    // CUANDO SE USE EL COMPONENTE, SE VA TRAER LA LISTA DE USUARIOS
+    // CUANDO SE USE EL COMPONENTE, SE VA TRAER LA LISTA DE productos
     useEffect( () => {
         console.log("Fetch de usuarios")
     } ,[])
@@ -30,20 +32,19 @@ function ProductListComponent() {
                     <div className="col-md-9 col-xl-10  ">
                         {/* Button Section */}
                         <section className='d-flex justify-content-center m-3'>
-                            <h2>Productos</h2>
                             <button type="button" className="btn btn-primary mx-3 fw-bold btn-lg" onClick={newProduct}>Nuevo</button>
-                            <button type="button" className="btn btn-primary mx-3 fw-bold btn-lg" onClick={editProduct}>Editar</button>
                             <button type="button" className="btn btn-primary mx-3 fw-bold btn-lg" onClick={deleteProduct}>Eliminar</button>
                         </section>
 
                         {/* Table Section */}
-                        <section className='d-flex justify-content-center' style={{ maxHeight: '85vh', overflowY: 'auto' }}>
-                            <table className="table">
+                        <section className='d-flex justify-content-center rounded-3'  style={{ maxHeight: '85vh', overflowY: 'auto' }}>
+                            <table className="table table-primary">
                                 <thead style={{ position: 'sticky', top: 0, borderBottom: '2px solid black' }}>
                                     <tr>
                                         <th scope="col">#</th>
-                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Nombre Producto</th>
                                         <th scope="col">Descripción</th>
+                                        <th scope="col">Precio</th>
                                         <th scope="col">Estado</th>
                                         <th scope="col">#</th>
                                     </tr>
@@ -58,6 +59,15 @@ function ProductListComponent() {
                                         <td></td>
                                         <td></td>
                                         <td></td>
+                                        <td>
+
+                                            {/* Icono */}
+                                            <FontAwesomeIcon
+                                                icon={faPenToSquare}
+                                                style={{ color: "#000000", }}
+                                                
+                                            />
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
