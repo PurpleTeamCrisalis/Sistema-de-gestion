f996dd6405e320dae5ba9ad04ffcc9bd0e2435d6
Julieta Tatiana <julietacastillo326@gmail.com>
2023-10-11 10:38:27 -0300

    Creando router de Home y Login

--- backoffice-Front-End/backoffice-web/src/App.js
+++ backoffice-Front-End/backoffice-web/src/App.js
@@ -1,10 +1,15 @@
 import HomePage from "./pages/HomePage";
+import {LoginPage} from "./pages/LoginPage"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+      <BrowserRouter> 
+        <Routes>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/login" element={<LoginPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
