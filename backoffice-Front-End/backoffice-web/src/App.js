05e8c8aaefcdfc197ebc1b80363ff7f2c06f7511
Julieta Tatiana <julietacastillo326@gmail.com>
2023-10-11 11:59:59 -0300

    Creación del Router del home-nav-grilla

--- backoffice-Front-End/backoffice-web/src/App.js
+++ backoffice-Front-End/backoffice-web/src/App.js
@@ -1,6 +1,12 @@
 import HomePage from "./pages/HomePage";
 import {LoginPage} from "./pages/LoginPage"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserListComponent from "./components/ListComponents/UserListComponent";
+import ServiceListComponent from "./components/ListComponents/ServiceListComponent";
+import ProductListComponent from "./components/ListComponents/ProductListComponent";
+import OrderListComponent from "./components/ListComponents/OrderListComponent";
+import ClientListComponent from "./components/ListComponents/ClientListComponent";
+import ChargeListComponent from "./components/ListComponents/ChargeListComponent";
 
 function App() {
   return (
@@ -8,6 +14,12 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage/>}></Route>
           <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/user" element={<UserListComponent/>}></Route>
+          <Route path="/service" element={<ServiceListComponent/>}></Route>
+          <Route path="/product" element={<ProductListComponent/>}></Route>
+          <Route path="/order" element={<OrderListComponent/>}></Route>
+          <Route path="/client" element={<ClientListComponent/>}></Route>
+          <Route path="/charge" element={<ChargeListComponent/>}></Route>
         </Routes>
       </BrowserRouter>
   );
