d990ae9046706c2b626ac020d2270d526e8f8c70
Oscar-Sch <oscarschwerdt@gmail.com>
2023-10-16 17:03:01 -0300

    borre importaciones que no se usaban

--- backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/AuthController.java
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/AuthController.java
@@ -1,9 +1,6 @@
 package edu.bootcamp.backoffice.controller;
 
-import edu.bootcamp.backoffice.model.dto.AuthResponseDTO;
 import edu.bootcamp.backoffice.model.dto.UserDTO;
-import edu.bootcamp.backoffice.repository.UserRepository;
-import edu.bootcamp.backoffice.security.JWTGenerator;
 import edu.bootcamp.backoffice.security.SecurityConstants;
 import edu.bootcamp.backoffice.security.TokenBlacklist;
 import edu.bootcamp.backoffice.service.AuthService;
@@ -11,11 +8,7 @@ import edu.bootcamp.backoffice.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
