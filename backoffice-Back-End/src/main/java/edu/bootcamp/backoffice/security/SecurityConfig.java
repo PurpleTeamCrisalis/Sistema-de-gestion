3651afa7fa998ea12ccb9942c9752e937c8c5059
Oscar-Sch <oscarschwerdt@gmail.com>
2023-10-16 20:20:18 -0300

    inclusion de la documentacion

--- backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/security/SecurityConfig.java
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/security/SecurityConfig.java
@@ -8,20 +8,16 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private JwtAuthEntryPoint authEntryPoint;
-    private CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthEntryPoint authEntryPoint;
+    private final CustomUserDetailsService customUserDetailsService;
     @Autowired
     public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtAuthEntryPoint authEntryPoint) {
         this.customUserDetailsService = customUserDetailsService;
