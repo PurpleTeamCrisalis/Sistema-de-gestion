1ce2e6f0e2a801e9bb58ce0ef5f0df93d948e578
Oscar-Sch <oscarschwerdt@gmail.com>
2023-10-16 16:36:50 -0300

    Añadido el logout al AuthController

--- backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/AuthController.java
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/controller/AuthController.java
@@ -14,10 +14,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(path="/auth")
@@ -44,5 +45,13 @@ public class AuthController {
             return new ResponseEntity<>("User logged in successfully",authService.authenticateAndReturnHeader(userDTO), HttpStatus.OK);
         }
         return new ResponseEntity<>("Credentials do not correspond to a valid user", HttpStatus.NOT_FOUND);
+    }
+    @GetMapping(path="/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        SecurityContextHolder.clearContext();
+        return new ResponseEntity<>("User logged out successfully",HttpStatus.OK);
     }
 }
