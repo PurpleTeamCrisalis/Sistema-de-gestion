459ed775d13cf8b57b2ac6e9dbb17068a555577f
ivanbvn <bilvinas.ivan@gmail.com>
2023-10-17 13:57:03 -0300

    testing

--- backoffice-Front-End/backoffice-web/src/router/AppRouter.jsx
+++ backoffice-Front-End/backoffice-web/src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
@@ -8,17 +8,16 @@ import ProductListComponent from "../components/ListComponents/ProductListCompon
 import OrderListComponent from "../components/ListComponents/OrderListComponent";
 import ClientListComponent from "../components/ListComponents/ClientListComponent";
 import ChargeListComponent from "../components/ListComponents/ChargeListComponent";
-
-import { useSelector } from "react-redux";
+import NewUserComponent from "../components/NewElementComponents/NewUserComponent";
+import EditUserComponent from "../components/EditElementComponents/EditUserComponent";
+import { useAuthStore } from "../hooks";
 
 export const AppRouter = () => {
-  // const [status, setStatus] = useState("authenticated");
-
-  const status = useSelector((state) => state.auth);
+  const { status } = useAuthStore();
 
   return (
     <Routes>
-      {status.status === "authenticated" ? (
+      {status === "authenticated" ? (
         <>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<Navigate to="/" />} />
@@ -28,6 +27,8 @@ export const AppRouter = () => {
           <Route path="/order" element={<OrderListComponent />}></Route>
           <Route path="/client" element={<ClientListComponent />}></Route>
           <Route path="/charge" element={<ChargeListComponent />}></Route>
+          <Route path="/user/newUser" element={<NewUserComponent />}></Route>
+          <Route path="/user/editUser" element={<EditUserComponent />}></Route>
         </>
       ) : (
         <>
