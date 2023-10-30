f014d5daf74b6916ea0997675861b11b6cdee4ff
Marcos Leiva <marcleiva623@gmail.com>
2023-10-30 10:02:32 -0300

    Token funcionando

--- backoffice-Front-End/backoffice-web/src/api/projectApi.js
+++ backoffice-Front-End/backoffice-web/src/api/projectApi.js
@@ -7,20 +7,20 @@ export const projectApi = axios.create({
   baseURL: API_URL
 })
 
-// projectApi.interceptors.response.use(response => {
-//   localStorage.setItem('token', response.headers['authorization'])
-//   return response
-// }, error => {
-//   console.log(error.response.headers['authorization'])
-//   localStorage.setItem('token', error.response.headers['authorization'])
-// })
+projectApi.interceptors.response.use(response => {
+  localStorage.setItem('token', response.headers['authorization'])
+  return response
+}, error => {
+  console.log(error.response.headers['authorization'])
+  localStorage.setItem('token', error.response.headers['authorization'])
+})
 
-// projectApi.interceptors.request.use(config => {
-//   config.headers = {
-//     ...config.headers,
-//     'Content-Type': 'application/json',
-//     'authorization': localStorage.getItem('token'),
-//     // 'Origin' : 'http://localhost:3000'
-//   }
-//   return config
-// })
+projectApi.interceptors.request.use(config => {
+  config.headers = {
+    ...config.headers,
+    'Content-Type': 'application/json',
+    'authorization': localStorage.getItem('token'),
+    // 'Origin' : 'http://localhost:3000'
+  }
+  return config
+})
