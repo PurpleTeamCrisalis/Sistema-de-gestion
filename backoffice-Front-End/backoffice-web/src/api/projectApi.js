0dfa412b459870f4aeda8234db949fd388f24858
ivanbvn <bilvinas.ivan@gmail.com>
2023-11-01 15:24:51 -0300

    Eliminacion de Console.logs en projectApi.js

--- backoffice-Front-End/backoffice-web/src/api/projectApi.js
+++ backoffice-Front-End/backoffice-web/src/api/projectApi.js
@@ -11,7 +11,6 @@ projectApi.interceptors.response.use(response => {
   localStorage.setItem('token', response.headers['authorization'])
   return response
 }, error => {
-  console.log(error.response.headers['authorization'])
   localStorage.setItem('token', error.response.headers['authorization'])
 })
 
@@ -20,7 +19,6 @@ projectApi.interceptors.request.use(config => {
     ...config.headers,
     'Content-Type': 'application/json',
     'authorization': localStorage.getItem('token'),
-    // 'Origin' : 'http://localhost:3000'
   }
   return config
 })
