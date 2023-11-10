b68b433e0b53837796d1fd013a14e19c65d9dd43
ivanbvn <bilvinas.ivan@gmail.com>
2023-11-10 18:15:46 -0300

    [Agregado] Estructuración para nuevos datos de pedido que llegaran desde el Back

--- backoffice-Front-End/backoffice-web/src/components/Modal/DetailModal.jsx
+++ backoffice-Front-End/backoffice-web/src/components/Modal/DetailModal.jsx
@@ -13,7 +13,7 @@ export const DetailModal = () => {
       aria-labelledby="staticBackdropLabel"
       aria-hidden="true"
     >
-      <div className="modal-dialog">
+      <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className="modal-header">
             <h1 className="modal-title fs-5" id="staticBackdropLabel">
@@ -40,20 +40,36 @@ export const DetailModal = () => {
                         borderBottom: "2px solid black",
                       }}
                     >
-                      <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Cantidad</th>
-                        <th scope="col">Precio Unitario</th>
-                        <th scope="col">Subtotal</th>
+                      <tr style={{ textAlign: "center" }}>
+                        <th scope="col" width="20%">
+                          Nombre
+                        </th>
+                        <th scope="col" width="10%">
+                          Cantidad
+                        </th>
+                        <th scope="col" width="20%">
+                          Precio Unitario
+                        </th>
+                        <th scope="col" width="25%">
+                          Impuestos
+                        </th>
+                        <th scope="col" width="10%">
+                          %
+                        </th>
+                        <th scope="col" width="20%">
+                          Subtotal
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
                       {selectedOrder?.products.map((product) => (
-                        <tr key={product.id}>
+                        <tr key={product.id} style={{ textAlign: "center" }}>
                           <td>{product.name}</td>
                           <td>{product.quantity}</td>
-                          <td>{product.basePrice}</td>
-                          <td>{product.subTotal}</td>
+                          <td>${product.basePrice}</td>
+                          <td>{product.taxesApplied}</td>
+                          <td>{product.taxCharges}%</td>
+                          <td>${product.subTotal}</td>
                         </tr>
                       ))}
                     </tbody>
@@ -74,18 +90,32 @@ export const DetailModal = () => {
                         borderBottom: "2px solid black",
                       }}
                     >
-                      <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Precio Unitario</th>
-                        {/* <th scope="col">Subtotal</th> */}
+                      <tr style={{ textAlign: "center" }}>
+                        <th scope="col" width="20%">
+                          Nombre
+                        </th>
+                        <th scope="col" width="25%">
+                          Precio Unitario
+                        </th>
+                        <th scope="col" width="30%">
+                          Impuestos
+                        </th>
+                        <th scope="col" width="10%">
+                          %
+                        </th>
+                        <th scope="col" width="20%">
+                          Subtotal
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
                       {selectedOrder?.services.map((service) => (
-                        <tr key={service.id}>
+                        <tr key={service.id} style={{ textAlign: "center" }}>
                           <td>{service.name}</td>
-                          <td>{service.basePrice}</td>
-                          {/* <td>{service.subTotal}</td> */}
+                          <td>${service.basePrice}</td>
+                          <td>{service.taxesApplied}</td>
+                          <td>{service.taxCharges}%</td>
+                          <td>${service.subTotal}</td>
                         </tr>
                       ))}
                     </tbody>
@@ -96,13 +126,28 @@ export const DetailModal = () => {
               )}
             </div>
           </div>
-          <div className="modal-footer">
+          <div
+            className="modal-footer"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button
+              type="button"
+              className="btn btn-danger"
+              // data-bs-dismiss="modal"
+              onClick={() => {alert('Orden cancelada')}} // Llamar a la funcion para que cambie el estado de la orden a cancelado.
+            >
+              Cancelar orden
+            </button>
             <button
               type="button"
               className="btn btn-primary"
-              data-bs-dismiss="modal"
+              // data-bs-dismiss="modal"
+              onClick={() => {alert('Orden pagada')}} // Llamar a la funcion para que cambie el estado de la orden a pagado.
             >
-              Cerrar
+              Pagar orden
             </button>
           </div>
         </div>
