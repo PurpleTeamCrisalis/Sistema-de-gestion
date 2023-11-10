1accc93bdf213d1370c9b79b6afc51376a07ee02
ivanbvn <bilvinas.ivan@gmail.com>
2023-11-10 18:14:07 -0300

    [Fix] Habia 2 userSlice.js

--- backoffice-Front-End/backoffice-web/src/redux/store.js
+++ backoffice-Front-End/backoffice-web/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { ordersSlice } from './orders/ordersSlice'
 import { newOrderSlice } from './newOrder/newOrderSlice'
 import { authSlice } from "./auth/authSlice";
-import { userSlice } from "./users/usersSlice";
+import { usersSlice } from "./users/usersSlice";
 import { clientsSlice } from "./client/clientsSlice";
 import { chargesSlice } from './charges/chargesSlice'
 import { productsSlice } from './products'
@@ -12,13 +12,12 @@ const rootReducer = combineReducers({
   auth: authSlice.reducer,
   orders: ordersSlice.reducer,
   newOrder: newOrderSlice.reducer,
-  users: userSlice.reducer,
+  users: usersSlice.reducer,
   clients: clientsSlice.reducer,
   charges: chargesSlice.reducer,
   products: productsSlice.reducer,
   services: servicesSlice.reducer,
 })
-
 
 export const store = configureStore({
   reducer: rootReducer,
