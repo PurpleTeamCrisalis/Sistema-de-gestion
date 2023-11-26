0c32a46bba03db40a6721c3bbd57c0866ae60b4f
Facundo Lozano <videoclipbanda2019@gmail.com>
2023-11-26 12:34:00 -0300

    Interceptor de la state machine

--- backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/repository/OrderStateRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.bootcamp.backoffice.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.bootcamp.backoffice.model.order.*;
-public interface OrderStateRepository extends JpaRepository<Order, Integer> {
-
-}
---
+++ backoffice-Back-End/src/main/java/edu/bootcamp/backoffice/service/OrderStateChangeInterceptor.java
@@ -0,0 +1,38 @@
+package edu.bootcamp.backoffice.service;
+
+import java.util.Optional;
+
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
+import org.springframework.stereotype.Component;
+
+import edu.bootcamp.backoffice.model.order.*;
+import edu.bootcamp.backoffice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class OrderStateChangeInterceptor extends StateMachineInterceptorAdapter<OrderState, OrderStateEvent> {
+
+	private final OrderRepository orderRepository;
+
+	@Override
+	public void preStateChange(State<OrderState, OrderStateEvent> state, Message<OrderStateEvent> message,
+			Transition<OrderState, OrderStateEvent> transition, StateMachine<OrderState, OrderStateEvent> stateMachine,
+			StateMachine<OrderState, OrderStateEvent> rootStateMachine) {
+		// TODO Auto-generated method stub
+		Optional.ofNullable(message).ifPresent(msg -> {
+			Optional.ofNullable(
+					Integer.class.cast(msg.getHeaders().getOrDefault(OrderServiceImpl.ORDER_ID_HEADER, -1L)))
+					.ifPresent(id -> {
+						Order order = orderRepository.getOne(id);
+						order.setOrderState(state.getId());
+						orderRepository.save(order);
+					});
+		});
+	}
+
+}
